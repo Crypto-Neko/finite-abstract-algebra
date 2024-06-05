25fe38dee623cefb5b611e960998c328bbdde1b8
Math Akemi <objective.axioma@gmail.com>
2024-06-05 11:16:36 -0400

    Removed __pycache_-

--- Number Theory/Integer Groups/Z2xZ2.py
+++ Number Theory/Integer Groups/Z2xZ2.py
@@ -1,6 +1,7 @@
 import sys; sys.path.append(r"../../Algebraic Structures")
 from bin_op import *
 from group import *
+from fin_ab_group import *
 
 x0 = Element('(0, 0)')
 x1 = Element('(1, 0)')
@@ -8,4 +9,4 @@ x2 = Element('(0, 1)')
 x3 = Element('(1, 1)')
 elements = [x0, x1, x2, x3]
 table = [[x0, x1, x2, x3], [x1, x0, x3, x2], [x2, x3, x0, x1], [x3, x2, x1, x0]]
-Z2xZ2 = Group(elements, table)
+Z2xZ2 = FinAbGroup(elements, table)
