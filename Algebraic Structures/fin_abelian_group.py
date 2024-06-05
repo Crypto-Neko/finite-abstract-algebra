2903db849fd3dc477978808ca611189332ec4fb8
Math Akemi <objective.axioma@gmail.com>
2024-06-05 11:14:22 -0400

    Added a class to represent an element of a finite abelian group to replace multiplication with addition, since this is the standard representation of the operation defined on elements of a finite abelian group.

--- Algebraic Structures/fin_abelian_group.py
+++ Algebraic Structures/fin_abelian_group.py
@@ -2,6 +2,27 @@ from bin_op import *
 from group import *
 
 ####################################################################################################################
+# Represents an element of a FinAbGroup object. The only additional function is to replace multiplication with     #
+# additon, since the operation of an abelian group is usually represneted with a "+" sign.                         #
+#                                                                                                                  #
+# ElementOfFAG.value: The value associated with the element, a tuple.                                              #
+# ElementOfFAG.structure: The FinAbGroup object of which the ElementOfFAG object is an element.                    #
+####################################################################################################################
+class ElementOfFAG(Element):
+    def __init__(self, value, structure):
+        super().__init__(value, structure)
+
+    # Override the __mul__ method in Element so that it doesn't work anymore.
+    def __mul__(self, other):
+        raise ValueError("Multiplication not defined.")
+
+    # Use addition as the operation instead of multiplication because it's a FAG.
+    def __add__(self, other):
+        if isinstance(other, ElementOfFAG):
+            return self.structure.multiply(self, other)
+
+
+####################################################################################################################
 # Implements a finite abelian group with an arbitrary number of generators. Constructs the FinAbGroup object with  #
 # inheritance from the Group class. Takes as input a list of the order of the generators for the group, e.g.       #
 # Z2xZ3 = FinAbGroup([2, 3]).                                                                                      #
@@ -32,7 +53,7 @@ class FinAbGroup(Group):
     # Construct the elements of the finite abelian group recursively.
     def construct_elements(self, tup, i):
         if i == len(self.generators) - 1:
-            self.elements.append(Element(tup, self))
+            self.elements.append(ElementOfFAG(tup, self))
         else:
             for val in range(self.generators[i+1]):
                 self.construct_elements(tup + (val,), i + 1)
@@ -47,14 +68,21 @@ class FinAbGroup(Group):
                 tup = tuple()
                 for k in range(len(self.generators)):
                     tup += ((self.elements[i].value[k] + self.elements[j].value[k]) % self.generators[k]),
-                row.append(Element(tup, self))
+                row.append(ElementOfFAG(tup, self))
             table.append(row)
 
         self.table = table
 
 
-    # Print the elements of the group.
+    # Print the identify of the group in terms of a product of integer groups.
     def __str__(self):
-        return(str(self.elements))
+        rep = ""
+        for i in range(len(self.generators)):
+            rep += "Z" + str(self.generators[i])
+            if i != len(self.generators) - 1:
+                rep += " x "
+
+        return rep
+
 
     __repr__ = __str__
